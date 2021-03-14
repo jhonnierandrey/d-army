7942f998542211801e97aa15f12f8b6aa9ce811c
jhonnierandrey <jhonnierandrey@outlook.com>
2021-03-13 23:50:40 -0300

    prct on python

--- python/app.py
+++ python/app.py
@@ -1,55 +1,121 @@
-import tkinter as tk
-from tkinter import filedialog, Text
-import os
-import subprocess
+# variables
+age_sample = 20
+price = 19.99
+first_name = 'Batman'
+is_online = True
 
-root = tk.Tk()
-apps = []
+# inputs from user
+name = input('What is your name? ')
+print('Hello ' + name)
 
-if os.path.isfile('save.txt'):
-    with open('save.txt', 'r') as f:
-        tempApps = f.read()
-        tempApps = tempApps.split(',')
-        apps = [x for x in tempApps if x.strip()]
+birth_year = input('Enter your birth year: ')
+age = 2021 - int(birth_year)
+print(age)
 
-def addApp():
+# build int functions
 
-    for widget in frame.winfo_children():
-        widget.destroy()
-    
-    filename = filedialog.askopenfilename(initialdir="/", title="Select File",
-                filetypes=(("executables", "*.app"), ("all files", "*.*")))
-    apps.append(filename)
-    print(filename)
-    for app in apps:
-        label = tk.Label(frame, text=app, bg="gray")
-        label.pack()
+# int()
+# float()
+# bool()
+# str()
 
-def runApps():
-    for app in apps:
-        # os.startfile(app)
-        subprocess.call(["/usr/bin/open", app])
+# simple calculator
 
-canvas = tk.Canvas(root, height=500, width=500, bg="#263D42")
-canvas.pack()
+first_number = input('First: ')
+second_number = input('Second: ')
+print('Sum ' + str(float(first_number) + float(second_number)))
 
-frame = tk.Frame(root, bg="#FFFFFF")
-frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
+# strings
 
-openFile = tk.Button(root, text="Open File", padx=10,
-                    pady=5, bg="#263D42", fg="#000000", command=addApp)
-openFile.pack()
+course = 'Python'
 
-runApps = tk.Button(root, text="Run Apps", padx=10,
-                    pady=5, bg="#263D42", fg="#000000", command=runApps)
-runApps.pack()
+print(course.upper())
+print(course.lower())
+print(course.find('y'))
+print(course.replace('o', '0'))
 
-for app in apps:
-    label = tk.Label(frame, text=app)
-    label.pack()
+print('Python' in course)
 
-root.mainloop()
+# arithmetic operations
 
-with open('save.txt', 'w') as f:
-    for app in apps:
-        f.write(app + ',')
+x = 10
+x += 3
+print(x)
+
+# operator prededence
+
+x = (10 + 3) * 2
+print(x)
+
+# comparison operators
+x = 3 != 2
+print(x)
+
+# logical operators
+price = 5
+print(price > 10 and price < 30)
+print(price > 10 or price < 30)
+print(not price > 10)
+
+# if statements
+temperature = 5
+
+if temperature > 30:
+    print("It's a hot day")
+    print("Drink plenty of water")
+elif temperature > 20:
+    print("It's a nice day.")
+elif temperature > 10:
+    print("It's a bit cold.")
+else:
+    print("It's cold")
+print('Done')
+
+# weight exercise
+weight = float(input('Weight: '))
+measure = input("(K)g or (L)bs: ")
+
+if measure == 'l' or measure == 'L':
+    print("Weight in Kg: " + str(weight * 0.45))
+elif measure == 'k' or measure == 'K':
+    print("Weight in Lbs: " + str(weight / 0.45))
+
+# while loops
+i = 1 
+while i <= 5:
+    print(i * '*')
+    i += 1
+
+# lists 
+names = ["Jhon", "Bob", "Mosh", "Sam", "Mary"]
+names[0] = "Jon"
+
+# print(names[-2])
+print(names[0:3])
+
+# list methods
+numbers = [ 1, 2, 3, 4, 5]
+numbers.append(6)
+numbers.insert(0, "one")
+numbers.remove(3)
+# numbers.clear()
+
+print(len(numbers))
+
+# for loops
+numbers = [ 1, 2, 3, 4, 5]
+for item in numbers:
+    print(item)
+
+
+# the range() function
+numbers = range(5, 10)
+
+print(numbers)
+for number in numbers:
+    print(number)
+
+#tuples - unmutable 
+
+numbers = (1, 2, 3)
+numbers[0] = 5
