c4fb327bb6ebced02423dbe56ce06df073624193
jhonnierandrey <jhonnierandrey@outlook.com>
2021-05-06 00:42:44 -0300

    prct volume.c

--- C/week4/lab4/volume.c
+++ C/week4/lab4/volume.c
@@ -34,8 +34,20 @@ int main(int argc, char *argv[])
     float factor = atof(argv[3]);
 
     // TODO: Copy header from input file to output file
+    uint8_t header[HEADER_SIZE];
+    
+    fread(header, HEADER_SIZE, 1, input);
+    fwrite(header, HEADER_SIZE, 1, output);
 
     // TODO: Read samples from input file and write updated data to output file
+    int16_t buffer;
+    
+    while (fread(&buffer, 2, 1, input))
+    {
+        buffer *= factor;
+        fwrite(&buffer, 2, 1, output);
+    }
+
 
     // Close files
     fclose(input);
