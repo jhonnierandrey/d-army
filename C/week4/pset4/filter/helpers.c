cca0fbfdd3b59775123b28af974bc7ff47e0d776
jhonnierandrey <jhonnierandrey@outlook.com>
2021-05-15 23:36:16 -0300

    new problem set

---
+++ C/week4/pset4/filter/helpers.c
@@ -0,0 +1,68 @@
+#include "helpers.h"
+#include <stdio.h>
+#include <math.h>
+
+// Convert image to grayscale
+void grayscale(int height, int width, RGBTRIPLE image[height][width])
+{
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            // get average 
+            int average = image[i][j].rgbtRed + image[i][j].rgbtGreen + image[i][j].rgbtBlue;
+
+            // return new image with new values
+            image[i][j].rgbtRed = round(average / 3);
+            image[i][j].rgbtGreen = round(average / 3);
+            image[i][j].rgbtBlue = round(average / 3);
+        }
+    }
+    
+    return;
+}
+
+// Convert image to sepia
+void sepia(int height, int width, RGBTRIPLE image[height][width])
+{
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            // calculate sepia values
+            int sepiaRed = .393 * image[i][j].rgbtRed + .769 * image[i][j].rgbtGreen + .189 * image[i][j].rgbtBlue;
+            int sepiaGreen = .349 * image[i][j].rgbtRed + .686 * image[i][j].rgbtGreen + .168 * image[i][j].rgbtBlue;
+            int sepiaBlue = .272 * image[i][j].rgbtRed + .534 * image[i][j].rgbtGreen + .131 * image[i][j].rgbtBlue;
+
+            if(sepiaRed > 255)
+            {
+                sepiaRed = 255;
+            }
+            if(sepiaGreen > 255)
+            {
+                sepiaGreen = 255;
+            }
+            if(sepiaBlue > 255)
+            {
+                sepiaBlue = 255;
+            }
+            // return new image with new values
+            image[i][j].rgbtRed = round(sepiaRed);
+            image[i][j].rgbtGreen = round(sepiaGreen);
+            image[i][j].rgbtBlue = round(sepiaBlue);
+        }
+    }
+    return;
+}
+
+// Reflect image horizontally
+void reflect(int height, int width, RGBTRIPLE image[height][width])
+{
+    return;
+}
+
+// Blur image
+void blur(int height, int width, RGBTRIPLE image[height][width])
+{
+    return;
+}
