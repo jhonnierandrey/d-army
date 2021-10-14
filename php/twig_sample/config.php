a5fd3d058c3d2d7e2584fd6408f5ff110af4acbc
jhonnierandrey <jhonnierandrey@outlook.com>
2021-10-14 20:13:16 -0300

    removing unused dir

--- php/twig_sample/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once __DIR__ . '/vendor/autoload.php';
-
-define('APP_ROOT', __DIR__);
-
-$loader = new \Twig\Loader\FilesystemLoader(APP_ROOT . '/views');
-$twig = new \Twig\Environment($loader);
