b341b1b6f94bbfe25e7348ebf77c8df03082fe8c
jhonnierandrey <jhonnierandrey@outlook.com>
2021-10-14 21:07:21 -0300

    prx with vue

--- vue/cac/js/script.js
+++ vue/cac/js/script.js
@@ -1,6 +1,14 @@
 const app = new Vue({
     el: '#root',
     data: {
-        msg : 'Getting Started with Vue'
+        msg : 'Getting Started with Vue',
+        framework : 'Vue',
+        language : 'JS',
+        htmlcontent : '<button> Start Now </button>'
+    },
+    methods : {
+        details : function(){
+            return `Welcome to ${this.framework} ${this.language}`;
+        }
     }
 })
