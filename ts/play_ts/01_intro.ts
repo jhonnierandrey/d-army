5f9a5235b3c02ffe51c133bcb57f85cb7b54d307
jhonnierandrey <jhonnierandrey@outlook.com>
2021-11-27 17:33:16 -0300

    prct with TS

--- ts/play_ts/01_intro.ts
+++ ts/play_ts/01_intro.ts
@@ -1,1 +1,11 @@
-// Typescript main goal is in writing Safe Code whereas in ES6 is in writing Less Code.
+/*
+Typescript main goal is in writing Safe Code whereas in ES6 is in writing Less Code.
+
+TS vs JS
+Adventages of TS:
+
+1 - Optional static typing
+2 - Improved readability.
+3 - Intellisense
+4 - New features
+*/
